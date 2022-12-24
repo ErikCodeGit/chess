019a437502578d95ea7432460cbb6ee0e7981059
ErikCodeGit <erik.mueller0912@gmail.com>
2022-12-24 16:52:34 +0100

    Implement check, checkmate and stalemate

--- lib/test.rb
+++ lib/test.rb
@@ -2,14 +2,20 @@
 
 require './lib/board'
 require './lib/display'
+require './lib/player'
 include Display
 Dir['./lib/pieces/*.rb'].sort.each { |file| require file }
 
 @board = Board.new
-@board.set_piece_at([2, 2], Bishop.new(:white, [2, 2], @board))
-bishop = @board.piece_at([2, 2])
+@board.set_piece_at([0, 0], King.new(:white, [0, 0], @board))
+@board.set_piece_at([1, 2], King.new(:black, [1, 2], @board))
+@board.set_piece_at([1, 1], Rook.new(:black, [1, 1], @board))
+@board.white_king = @board.piece_at([0, 0])
+@board.black_king = @board.piece_at([1, 2])
 display_board
-puts move_array_to_s(bishop.movement_pattern, bishop)
-puts move_array_to_s(bishop.valid_moves, bishop)
-
-p [nil, nil, nil].compact
+pablo = Player.new(:white, 1, 'pablo')
+gus = Player.new(:black, 2, 'gus')
+pablo.king = @board.white_king
+gus.king = @board.black_king
+p @board.stalemate?(pablo)
+p @board.white_king.valid_moves
